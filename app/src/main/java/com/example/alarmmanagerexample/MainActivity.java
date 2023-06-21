13e47c65b2c21f1cd4efdb14308ca43f6eb3383f
Guru Prakash Kumar <guruprakash745@gmail.com>
2023-06-21 17:18:33 +0530

    final commit when time is set and app is destroyed then at the time of alarm triggering, media just plays for 1-2 seconds

--- app/src/main/java/com/example/alarmmanagerexample/MainActivity.java
+++ app/src/main/java/com/example/alarmmanagerexample/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.alarmmanagerexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int ALARM_REQ_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 int time = Integer.parseInt(editText.getText().toString());
                 long triggerTime = System.currentTimeMillis()+(time* 1000L);
-                alarmManager.set(AlarmManager.RTC_WAKEUP,);
+                Intent iBroadcast = new Intent(MainActivity.this,MyReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,ALARM_REQ_CODE,iBroadcast,PendingIntent.FLAG_UPDATE_CURRENT);
+                alarmManager.set(AlarmManager.RTC_WAKEUP,triggerTime,pendingIntent);
             }
         });
 
